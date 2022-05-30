d3bf55aa76a20366a0c7c1734d0f1423a08e3c8f
maxme7 <77144011+maxme7@users.noreply.github.com>
2022-05-30 02:14:46 +0200

    fragments, navgraph, drawer

--- app/src/main/java/com/example/keepit/MainActivity.kt
+++ app/src/main/java/com/example/keepit/MainActivity.kt
@@ -15,95 +15,74 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import java.util.*
 import kotlin.collections.ArrayList
 
-private const val defaultUrl: String = "https://de.langenscheidt.com/deutsch-arabisch/gehen"
-//private const val defaultUrl: String = "https://de.langenscheidt.com/deutsch-englisch/bow"
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-    private lateinit var button: ImageButton
-    private lateinit var editText: EditText
-    private var url: String? = null
-    private var scrollY: Int = 0
+//    private lateinit var button: ImageButton
+//    private lateinit var editText: EditText
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfig: AppBarConfiguration
+
+    private lateinit var destinationChangeListener: NavController.OnDestinationChangedListener
 
     private val bookmarks: MutableList<String> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = findViewById(R.id.drawer_layout)
+        NavigationUI.setupWithNavController(findViewById<NavigationView>(R.id.navigationView), navController)
 
-        //setup
-        webView = findViewById(R.id.webView) //view binding?
-        button = findViewById(R.id.button)
-        editText = findViewById(R.id.editText)
+        appBarConfig = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfig)
 
-        webView.webViewClient = CustomWebViewClient()
+        destinationChangeListener = NavController.OnDestinationChangedListener{ controller, destination, arguments ->
 
-
-        Log.i("WEB", url.toString())
-//        webView.loadUrl(url ?: "https://de.langenscheidt.com/deutsch-arabisch/gehen")
-
-        //browser settings
-        //https://stackoverflow.com/questions/8298237/video-not-appearing-on-a-webview
-        val settings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.javaScriptCanOpenWindowsAutomatically = false
-        settings.setSupportMultipleWindows(false)
-        settings.allowFileAccess = true
-        settings.loadWithOverviewMode = true
-        settings.setSupportZoom(true)
-        settings.builtInZoomControls = true
-        settings.displayZoomControls = false
-
-        webView.isVerticalScrollBarEnabled = false
-        webView.isHorizontalScrollBarEnabled = false
-        WebView.setWebContentsDebuggingEnabled(true)
-
-        //search button
-        button.setOnClickListener {
-            Snackbar.make(findViewById(R.id.coordinatorLayout), editText.text.toString(), 1000)
-                .show()
-            webView.loadUrl(editText.text.toString())
-            webView
         }
 
-        //https://stackoverflow.com/questions/39831360/webview-detect-button-click-event
-        //https://www.youtube.com/watch?v=9RwJeocTgJg
 
-        val injectionObject = InjectionObject()
-        webView.addJavascriptInterface(CustomJavascriptInterface(webView, injectionObject), "android") //TODO companion object in jsInterface?
-        //second parameter refers to the global window object which has a method PerformClick() that can be invoked
-
-
-        //search on enter key
-        //https://stackoverflow.com/questions/31378621/keylistener-on-edittext-android
-        editText.setOnKeyListener { v, keyCode, event ->
-            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                button.performClick()
-            }
-            true
-        }
 
         //https://stackoverflow.com/questions/33698122/android-change-actionbar-title-text-color
-        val s = SpannableString(title)
-        s.setSpan(
-            ForegroundColorSpan(resources.getColor(R.color.dark_red)),
-            0,
-            title.length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        supportActionBar?.title = s
+//        val s = SpannableString(title)
+//        s.setSpan(
+//            ForegroundColorSpan(resources.getColor(R.color.dark_red)),
+//            0,
+//            title.length,
+//            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+//        )
+//        supportActionBar?.title = s
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(destinationChangeListener)
     }
 
     override fun onResume() {
         super.onResume()
+        navController.addOnDestinationChangedListener(destinationChangeListener)
+    }
 
-        //load restored url after state change or default url
-        webView.loadUrl(url ?: defaultUrl)
-        webView.scrollTo(0,scrollY)
+    override fun onSupportNavigateUp() :Boolean{
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -112,59 +91,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     //https://developer.android.com/training/appbar/actions
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.bookmarkButton -> {
-//            val s = editText.text.toString()
-            val s = webView.url.toString()
-            if (!bookmarks.contains(s)) {
-                bookmarks.add(s)
-            }
-            true
-        }
-        R.id.backButton -> {
-            webView.goBack()
-            true
-        }
-        R.id.forwardButton -> {
-            webView.goForward()
-            true
-        }
-        R.id.bookarmsButton -> {
-            showDialog()
-            true
-        }
-        else -> {
-            // If we got here, the user's action was not recognized.
-            // Invoke the superclass to handle it.
-            super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+//        R.id.bookmarkButton -> {
+////            val s = editText.text.toString()
+//            val s = webView.url.toString()
+//            if (!bookmarks.contains(s)) {
+//                bookmarks.add(s)
+//            }
+//            true
+//        }
+//        R.id.backButton -> {
+//            webView.goBack()
+//            true
+//        }
+//        R.id.forwardButton -> {
+//            webView.goForward()
+//            true
+//        }
+//        R.id.bookarmsButton -> {
+//            showDialog()
+//            true
+//        }
+//        else -> {
+//            // If we got here, the user's action was not recognized.
+//            // Invoke the superclass to handle it.
+//            super.onOptionsItemSelected(item)
+//        }
+//    }
 
     //https://protocoderspoint.com/android-alert-dialog-box-with-a-list-of-options/
-    private fun showDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Bookmarks")
-        builder.setItems(bookmarks.toTypedArray()) { dialog, which ->
-            editText.setText(bookmarks[which])
-            button.performClick()
-            dialog.dismiss()
-        }
+//    private fun showDialog() {
+//        val builder = AlertDialog.Builder(this)
+//        builder.setTitle("Bookmarks")
+//        builder.setItems(bookmarks.toTypedArray()) { dialog, which ->
+//            editText.setText(bookmarks[which])
+//            button.performClick()
+//            dialog.dismiss()
+//        }
+//
+//        builder.create().show()
+//    }
 
-        builder.create().show()
-    }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("url", webView.url)
-        outState.putInt("scrollY", webView.scrollY) //not perfect, but better. different layouts do not match with scrollY TODO
-    }
-
-    // restored after onStart() and before onResume()
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        url = savedInstanceState.getString("url")
-        scrollY = savedInstanceState.getInt("scrollY")
-    }
 }
 
 // save in bundle: https://medium.com/@evanbishop/saving-state-through-the-bundle-when-you-rotate-the-screen-d913422fe4f6
---
+++ app/src/main/java/com/example/keepit/WebViewFragment.kt
@@ -0,0 +1,89 @@
+package com.example.keepit
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+
+private const val defaultUrl: String = "https://de.langenscheidt.com/deutsch-arabisch/gehen"
+//private const val defaultUrl: String = "https://de.langenscheidt.com/deutsch-englisch/bow"
+
+class WebViewFragment : Fragment() {
+    private lateinit var webView: WebView
+    private var url: String? = null
+    private var scrollY: Int = 0
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val fragm = inflater.inflate(R.layout.fragment_web_view, container, false)
+
+        //setup
+        webView = fragm.findViewById(R.id.webView) //view binding?
+        webView.webViewClient = CustomWebViewClient()
+
+
+        Log.i("WEB", url.toString())
+//        webView.loadUrl(url ?: "https://de.langenscheidt.com/deutsch-arabisch/gehen")
+
+        //browser settings
+        //https://stackoverflow.com/questions/8298237/video-not-appearing-on-a-webview
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.javaScriptCanOpenWindowsAutomatically = false
+        settings.setSupportMultipleWindows(false)
+        settings.allowFileAccess = true
+        settings.loadWithOverviewMode = true
+        settings.setSupportZoom(true)
+        settings.builtInZoomControls = true
+        settings.displayZoomControls = false
+
+        webView.isVerticalScrollBarEnabled = false
+        webView.isHorizontalScrollBarEnabled = false
+        WebView.setWebContentsDebuggingEnabled(true)
+
+
+        //https://stackoverflow.com/questions/39831360/webview-detect-button-click-event
+        //https://www.youtube.com/watch?v=9RwJeocTgJg
+
+        val injectionObject = InjectionObject()
+        webView.addJavascriptInterface(CustomJavascriptInterface(webView, injectionObject), "android") //TODO companion object in jsInterface?
+        //second parameter refers to the global window object which has a method PerformClick() that can be invoked
+
+
+        //search on enter key
+        //https://stackoverflow.com/questions/31378621/keylistener-on-edittext-android
+//        editText.setOnKeyListener { v, keyCode, event ->
+//            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+//                button.performClick()
+//            }
+//            true
+//        }
+
+
+        return fragm
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //load restored url after state change or default url
+        webView.loadUrl(url ?: defaultUrl)
+        webView.scrollTo(0,scrollY)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("url", webView.url)
+        outState.putInt("scrollY", webView.scrollY) //not perfect, but better. different layouts do not match with scrollY TODO
+    }
+
+    // restored after onStart() and before onResume()
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        url = savedInstanceState?.getString("url")
+        scrollY = savedInstanceState?.getInt("scrollY") ?: 0
+    }
+}
